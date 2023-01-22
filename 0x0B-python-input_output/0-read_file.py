877cd19c97b0c1ca7c57e12b511bc2e0b9b97536
asersuaveee <aserdeme1@gmail.com>
2023-01-22 12:11:43 +0000

    dd

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0-read_file.py
+"""
+
+
+def read_file(filename=""):
+    """
+        read file my_file_0.txt
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print("{}".format(file.read()), end="")
